d36374e3f9d2c3273fd637d062728c97e17a52a4
qubitrenegade <qubitrenegade@gmail.com>
2015-10-12 23:15:18 +0000

    fixed typo

--- cookbooks/apache/files/default/plugins/modules.rb
+++ cookbooks/apache/files/default/plugins/modules.rb
@@ -1,7 +1,7 @@
 Ohai.plugin(:Apache) do
   provides "apache/modules"
 
-  collect_data(:default) dp
+  collect_data(:default) do
     apache Mash.new
     modules = shell_out('apachectl -t -D DUMP_MODULES')
     apache[:modules] = modules.stdout
