bc840739b7af962392a74fc4c4af1c36eaf440be
qubitrenegade <qubitrenegade@gmail.com>
2015-10-13 22:21:53 +0000

    recipe and corresponding test to install mailx on different platforms.

--- cookbooks/mailx/recipes/default.rb
+++ cookbooks/mailx/recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+package 'install mailx' do
+  case node[:platform]
+  when 'redhat', 'cents'
+    package_name 'mailx'
+  when 'ubuntu', 'debian'
+    package_name 'mailutils'
+  end
+  action :install
+end
--- cookbooks/mailx/spec/unit/recipes/default_spec.rb
+++ cookbooks/mailx/spec/unit/recipes/default_spec.rb
@@ -7,6 +7,34 @@
 require 'spec_helper'
 
 describe 'mailx::default' do
+  context 'on Ubuntu' do
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new({ :platform => 'ubuntu',
+                                            :version => 14.04
+      })
+      runner.converge(described_recipe)
+    end
+    it 'should install the correct packages' do
+      expect(chef_run).to install_package('install mailx').with({ 
+                                            :package_name => 'mailutils'
+      })
+    end
+  end
+
+  context 'on Centos' do
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new({ :platform => 'centos',
+                                            :version => 6.5
+      })
+      runner.converge(described_recipe)
+    end
+    it 'should install the correct packages' do
+      expect(chef_run).to install_package('install mailx').with({
+                                            :package_name => 'mailx'
+      })
+    end
+  end
+
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new
