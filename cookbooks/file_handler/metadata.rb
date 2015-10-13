24dd663baf1dadb243ec2fea10be520a858550f3
qubitrenegade <qubitrenegade@gmail.com>
2015-10-13 20:11:05 +0000

    added file handler

---
+++ cookbooks/file_handler/metadata.rb
@@ -0,0 +1,7 @@
+name 'file_handler'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures file_handler'
+long_description 'Installs/Configures file_handler'
+version '0.1.0'
